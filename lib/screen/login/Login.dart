60e81bf6d9cf4c56072aee20c78a65e7e0beb6ef
suxin <shuxin.he@socialcredits.com>
2020-11-11 17:26:00 +0800

    添加登录页面

---
+++ lib/screen/login/Login.dart
@@ -0,0 +1,17 @@
+import "package:flutter/material.dart";
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            child: Text("login screen"),
+          )
+        ],
+      ),
+    );
+  }
+}
