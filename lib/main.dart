2911674e8ccb83e40db2003a39f0b31080657b7e
suxin1 <suxin1@live.com>
2020-11-18 16:42:46 +0800

    集成redux

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,29 @@
 import "package:flutter/material.dart";
 import "package:NeteaseMusicMobileFake/screen/home/Home.dart";
-import "package:NeteaseMusicMobileFake/screen/detail/Detail.dart";
-void main() {
-  runApp(MaterialApp(
-    title: "planets",
-    home: HomeScreen(),
-  ));
+// import "package:NeteaseMusicMobileFake/screen/detail/Detail.dart";
+import "package:NeteaseMusicMobileFake/store/count.dart";
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+
+class MyApp extends StatelessWidget {
+  final Store<CountState> store;
+
+  MyApp(this.store);
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreProvider<CountState>(
+      store: store,
+      child: MaterialApp(
+        title: "planets",
+        home: HomeScreen(),
+      ),
+    );
+  }
 }
 
+void main() {
+  final store =
+      Store<CountState>(reducer, initialState: CountState.initState());
+  runApp(MyApp(store));
+}
--- lib/screen/login/Login.dart
+++ lib/screen/login/Login.dart
@@ -1,8 +1,12 @@
+import 'package:NeteaseMusicMobileFake/store/count.dart';
 import "package:flutter/material.dart";
+import 'package:flutter_redux/flutter_redux.dart';
 import "package:flutter_svg/flutter_svg.dart";
+
 // import "package:redux/redux.dart";
 import "package:NeteaseMusicMobileFake/theme/default.dart";
 import "package:NeteaseMusicMobileFake/screen/login/Form.dart";
+import "package:NeteaseMusicMobileFake/store/count.dart" as countStore;
 
 class LoginScreen extends StatelessWidget {
   @override
@@ -28,9 +32,44 @@ class LoginScreen extends StatelessWidget {
               ),
             ),
             LoginForm(),
+            Counter()
           ],
         ),
       ),
+    );
+  }
+}
+
+class Counter extends StatelessWidget {
+  const Counter({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreConnector<CountState, int>(
+      converter: (store) => store.state.count,
+      builder: (context, count) {
+        return Row(
+          children: [
+            Text(
+              count.toString(),
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            StoreConnector<CountState, VoidCallback>(
+              converter: (store) {
+                return () => store.dispatch(countStore.Action.increment);
+              },
+              builder: (context, callback) {
+                return RaisedButton(
+                  child: Text("plus"),
+                  onPressed: callback,
+                );
+              },
+            )
+          ],
+        );
+      },
     );
   }
 }
